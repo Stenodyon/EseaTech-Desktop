d0dcf02c67baa1e502f67b55b8657d0a2126bc56
Kevin Le Run <lerunkevin@gmail.com>
2019-05-20 10:44:53 +0200

    Added serial port selection

--- src/eseatech/Controller.java
+++ src/eseatech/Controller.java
@@ -1,21 +1,20 @@
 package eseatech;
 
+import com.fazecast.jSerialComm.SerialPort;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Menu;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioMenuItem;
+import javafx.scene.control.ToggleGroup;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
-    private final String[] testPorts = {
-            "/dev/ttyUSB0",
-            "/dev/ttyACM0",
-    };
+    private SerialPort currentSerialPort = null;
 
     @FXML
     private Menu menu_serial_port;
@@ -36,10 +35,28 @@ public class Controller implements Initializable {
         generateSerialMenu();
     }
 
+    private void setCurrentSerialPort(SerialPort newSerialPort) {
+        if (currentSerialPort != null) {
+            currentSerialPort.closePort();
+        }
+        currentSerialPort = newSerialPort;
+        currentSerialPort.openPort();
+    }
+
     private void generateSerialMenu() {
+        ToggleGroup toggleGroup = new ToggleGroup();
+
         menu_serial_port.getItems().clear();
-        for (String port : testPorts) {
-            MenuItem menuItem = new MenuItem(port);
+        for (SerialPort port : SerialPort.getCommPorts())
+        {
+            String label = String.format("%s (%s)", port.getSystemPortName(), port.getPortDescription());
+
+            RadioMenuItem menuItem = new RadioMenuItem(label);
+            menuItem.setToggleGroup(toggleGroup);
+            menuItem.setOnAction(event -> {
+                setCurrentSerialPort(port);
+            });
+
             menu_serial_port.getItems().add(menuItem);
         }
     }
