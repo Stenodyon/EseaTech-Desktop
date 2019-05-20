088831b981de32c5876455419f91eacb3accf5f2
Kevin Le Run <lerunkevin@gmail.com>
2019-05-20 14:23:01 +0200

    Almost done implementing message passing with MessagePack

--- src/eseatech/Controller.java
+++ src/eseatech/Controller.java
@@ -1,8 +1,6 @@
 package eseatech;
 
 import com.fazecast.jSerialComm.SerialPort;
-import com.fazecast.jSerialComm.SerialPortDataListener;
-import com.fazecast.jSerialComm.SerialPortEvent;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -10,13 +8,19 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Menu;
 import javafx.scene.control.RadioMenuItem;
 import javafx.scene.control.ToggleGroup;
+import org.msgpack.core.MessagePack;
+import org.msgpack.core.MessageUnpacker;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
     public static SerialPort currentSerialPort = null;
+    private MessageUnpacker unpacker = null;
 
     @FXML
     private Menu menu_serial_port;
@@ -34,29 +38,72 @@ public class Controller implements Initializable {
     @FXML
     public void initialize(URL url, ResourceBundle resourceBundle) {
         generateSerialMenu();
+
+        Thread dataThread = new Thread(() -> {
+            while (true) {
+                if (unpacker != null) {
+                    System.out.print("unpacker is not null!\n");
+
+                    byte[] command = new byte[1];
+                    command[0] = 'a';
+                    currentSerialPort.writeBytes(command, command.length);
+                    try {
+                        Map<String, Float> valuemap = unpackMap(unpacker);
+                        System.out.printf("%s\n", valuemap.toString());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        dataThread.setDaemon(true);
+        dataThread.start();
     }
 
     private void setCurrentSerialPort(SerialPort newSerialPort) {
         if (currentSerialPort != null) {
+            unpacker = null;
             currentSerialPort.closePort();
         }
         currentSerialPort = newSerialPort;
         currentSerialPort.openPort();
-        currentSerialPort.addDataListener(new SerialPortDataListener() {
-            @Override
-            public int getListeningEvents() {
-                return SerialPort.LISTENING_EVENT_DATA_AVAILABLE;
-            }
+        currentSerialPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
+        unpacker = MessagePack.newDefaultUnpacker(currentSerialPort.getInputStream());
 
-            @Override
-            public void serialEvent(SerialPortEvent event) {
-                if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE)
-                    return;
-                byte[] data = new byte[currentSerialPort.bytesAvailable()];
-                int numRead = currentSerialPort.readBytes(data, data.length);
-                System.out.printf("Read %d bytes: \"%s\"\n", numRead, data.toString());
-            }
-        });
+//        currentSerialPort.addDataListener(new SerialPortDataListener() {
+//            @Override
+//            public int getListeningEvents() {
+//                return SerialPort.LISTENING_EVENT_DATA_AVAILABLE;
+//            }
+//
+//            @Override
+//            public void serialEvent(SerialPortEvent event) {
+//                if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE)
+//                    return;
+//                byte[] data = new byte[currentSerialPort.bytesAvailable()];
+//                int numRead = currentSerialPort.readBytes(data, data.length);
+//                System.out.printf("Read %d bytes: \"%s\"\n", numRead, data.toString());
+//            }
+//        });
+    }
+
+    private Map<String, Float> unpackMap(MessageUnpacker unpacker) throws IOException {
+        int length = unpacker.unpackMapHeader();
+        System.out.printf("New map of length %d\n", length);
+        Map<String, Float> map = new HashMap<>(length);
+        for (int i = 0; i < length; i++) {
+            String key = unpacker.unpackString();
+            System.out.printf("\tNew key \"%s\"\n", key);
+            float value = unpacker.unpackFloat();
+            System.out.printf("\twith value %f\n", value);
+            map.put(key, value);
+        }
+        return map;
     }
 
     private void generateSerialMenu() {
--- src/eseatech/DataProvider.java
+++ src/eseatech/DataProvider.java
@@ -1,5 +1,64 @@
 package eseatech;
 
+import com.fazecast.jSerialComm.SerialPort;
+import org.msgpack.core.MessagePack;
+import org.msgpack.core.MessageUnpacker;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 // Registers a callback to provide new data
-public class DataProvider<DataType> {
+public class DataProvider
+{
+    private static final byte REQUEST_COMMAND = 'r';
+
+    private SerialPort port;
+    private MessageUnpacker unpacker;
+
+    public DataProvider(SerialPort port) {
+        this.port = port;
+        port.openPort();
+        port.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
+        unpacker = MessagePack.newDefaultUnpacker(port.getInputStream());
+    }
+
+    public Map<String, Float>[] fetchData() throws IOException {
+        sendFetchRequest();
+        return unpackData();
+    }
+
+    private void sendFetchRequest() {
+        sendByte(REQUEST_COMMAND);
+    }
+
+    private void sendByte(byte value) {
+        byte[] command = new byte[1];
+        command[0] = value;
+        port.writeBytes(command, command.length);
+    }
+
+    private Map<String, Float>[] unpackData() throws IOException {
+        int length = unpacker.unpackArrayHeader();
+        Map<String, Float>[] entries = new Map[length];
+
+        for (int i = 0; i < length; i++) {
+            entries[i] = unpackMap(unpacker);
+        }
+
+        return entries;
+    }
+
+    private Map<String, Float> unpackMap(MessageUnpacker unpacker) throws IOException {
+        int length = unpacker.unpackMapHeader();
+        Map<String, Float> map = new HashMap<>(length);
+
+        for (int i = 0; i < length; i++) {
+            String key = unpacker.unpackString();
+            float value = unpacker.unpackFloat();
+            map.put(key, value);
+        }
+
+        return map;
+    }
 }
